f796567d5df2d3d1071dc8f4f07e2b3968b5c9fa
Dominik Korsa <dominik.korsa@gmail.com>
2023-08-24 12:12:17 +0200

    Add prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 /* eslint-env node */
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/strict-type-checked'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/strict-type-checked', 'prettier'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         tsconfigRootDir: __dirname,
