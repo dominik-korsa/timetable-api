b698b88b4f0f8b12c753fa7134654b3a48dba04f
Marioneq 4958 <mq4958@gmail.com>
2023-11-24 16:42:17 +0100

    Add checking school website

--- packages/schools/src/client.ts
+++ packages/schools/src/client.ts
@@ -1,7 +1,10 @@
 import { RspoApiClient } from './rspo/client.js';
+import { checkTimetablePage, findLinksByKeywords } from './utils.js';
 import { School } from './types.js';
 import { Institution as RspoApiInstitution } from './rspo/types.js';
 import fs from 'fs';
+import { AxiosInstance } from 'axios';
+import { JSDOM } from 'jsdom';
 
 export async function getSchools(institutionTypeIds: number[]): Promise<School[]> {
     const rspoClient = new RspoApiClient();
@@ -52,4 +55,36 @@ export async function getSchools(institutionTypeIds: number[]): Promise<School[]
     await fs.promises.writeFile('data.json', JSON.stringify(schools), 'utf8');
     console.log('The data has been saved as data.json');
     return schools;
+}
+
+export async function checkSchoolWebsite(
+    depthLimit: number,
+    url: string,
+    axios: AxiosInstance,
+    checkedLinks: string[] = [],
+) {
+    const timetables: { url: string; type: 'optivum' | 'asctimetables' }[] = [];
+    const response = await axios.get<string>(url.replace('www.', ''));
+    checkedLinks.push(url);
+    const document = new JSDOM(response.data).window.document;
+    const links = [
+        ...new Set(
+            findLinksByKeywords(document).map((link) =>
+                new URL(link, url.replace('www.', '')).toString(),
+            ),
+        ),
+    ];
+    if (depthLimit > 0) {
+        await Promise.all(
+            links.map(async (link) => {
+                if (checkedLinks.includes(link)) return;
+                checkedLinks.push(link);
+                const result = await checkSchoolWebsite(depthLimit - 2, link, axios, checkedLinks);
+                timetables.push(...result.timetables);
+            })
+        );
+    };
+    const timetableType = checkTimetablePage(window.document);
+    if (timetableType !== undefined) timetables.push({ url, type: timetableType });
+    return { url: url.replace('www.', ''), timetables: [...new Set(timetables)] };
 }
