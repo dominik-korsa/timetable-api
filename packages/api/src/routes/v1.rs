05c3c44a0597f270b36823e13e7f59267aea475a
Dominik Korsa <dominik.korsa@gmail.com>
2023-12-28 13:37:52 +0100

    Use serde for params

--- packages/api/src/routes/v1.rs
+++ packages/api/src/routes/v1.rs
@@ -1,35 +1,40 @@
-use std::collections::HashMap;
 use axum::extract::{Path, State};
 use axum::Json;
 use axum::response::IntoResponse;
 use phf::{phf_set, Set};
+use serde::Deserialize;
 use crate::db::{get_school_by_rspo_id, get_schools_by_voivodeship};
 use crate::error::ApiError;
 use crate::state::SharedState;
 
 const VALID_VOIVODESHIPS: Set<&str> = phf_set!{ "02", "04", "06", "08", "10", "12", "14", "16", "18", "20", "22", "24", "26", "28", "30", "32" };
 
+#[derive(Deserialize)]
+pub(crate) struct VoivodeshipParams {
+    voivodeship: String,
+}
+
 pub(crate) async fn list_schools_by_voivodeship(
     State(state): State<SharedState>,
-    Path(params): Path<HashMap<String, String>>
+    Path(params): Path<VoivodeshipParams>
 ) -> impl IntoResponse {
-    // TODO: Remove .expect
-    let voivodeship_teryt = params.get("voivodeship").expect("Param \"voivodeship\" should be present");
-    if !VALID_VOIVODESHIPS.contains(voivodeship_teryt) {
+    if !VALID_VOIVODESHIPS.contains(&params.voivodeship) {
         return Err(ApiError::UnknownVoivodeship);
     }
-    let schools = get_schools_by_voivodeship(&state.db_pool, voivodeship_teryt).await?;
+    let schools = get_schools_by_voivodeship(&state.db_pool, &params.voivodeship).await?;
     Ok(Json(schools))
+}
+
+#[derive(Deserialize)]
+pub(crate) struct SchoolParams {
+    rspo_id: i32
 }
 
 pub(crate) async fn get_school(
     State(state): State<SharedState>,
-    Path(params): Path<HashMap<String, String>>
+    Path(params): Path<SchoolParams>
 ) -> impl IntoResponse {
-    // TODO: Remove .expect and .unwrap
-    let rspo_id: i32 = params.get("rspo_id").expect("Param \"rspo_id\" should be present")
-        .parse().unwrap();
-    let Some(school) = get_school_by_rspo_id(&state.db_pool, rspo_id).await? else {
+    let Some(school) = get_school_by_rspo_id(&state.db_pool, params.rspo_id).await? else {
         return Err(ApiError::EntityNotFound)
     };
     Ok(Json(school))
