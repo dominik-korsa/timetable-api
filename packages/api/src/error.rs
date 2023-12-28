d51bbdf31c40c9bbef00c59675fb7063f558712a
Dominik Korsa <dominik.korsa@gmail.com>
2023-12-28 15:59:55 +0100

    Use JSON for error response

--- packages/api/src/error.rs
+++ packages/api/src/error.rs
@@ -1,5 +1,7 @@
 use axum::http::StatusCode;
+use axum::Json;
 use axum::response::{IntoResponse, Response};
+use serde::Serialize;
 
 type Result<T> = std::result::Result<T, ApiError>;
 
@@ -11,14 +13,37 @@ pub(crate) enum ApiError {
     RouteNotFound,
 }
 
+impl ApiError {
+    fn get_status_code(&self) -> StatusCode {
+        match self {
+            ApiError::DbError => StatusCode::INTERNAL_SERVER_ERROR,
+            ApiError::InvalidTerytCode => StatusCode::BAD_REQUEST,
+            ApiError::EntityNotFound => StatusCode::NOT_FOUND,
+            ApiError::RouteNotFound => StatusCode::NOT_FOUND,
+        }
+    }
+
+    fn get_message(&self) -> &'static str {
+        match self {
+            ApiError::DbError => "Unexpected database error",
+            ApiError::InvalidTerytCode => "Invalid TERYT code",
+            ApiError::EntityNotFound => "Entity not found",
+            ApiError::RouteNotFound => "Route not found",
+        }
+    }
+}
+
+#[derive(Serialize)]
+struct ApiErrorResponse {
+    message: &'static str,
+}
+
 impl IntoResponse for ApiError {
     fn into_response(self) -> Response {
-        match self {
-            ApiError::DbError => (StatusCode::INTERNAL_SERVER_ERROR, "Unexpected database error"),
-            ApiError::InvalidTerytCode => (StatusCode::BAD_REQUEST, "Invalid TERYT code"),
-            ApiError::EntityNotFound => (StatusCode::NOT_FOUND, "Entity not found"),
-            ApiError::RouteNotFound => (StatusCode::NOT_FOUND, "Route not found"),
-        }.into_response()
+        let response = ApiErrorResponse {
+            message: self.get_message()
+        };
+        (self.get_status_code(), Json(response)).into_response()
     }
 }
 
