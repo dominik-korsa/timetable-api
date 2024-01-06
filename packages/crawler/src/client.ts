8b2775d72368c6f9321d27bc25cc7aa2e9eac3e1
Marioneq 4958 <mq4958@gmail.com>
2024-01-06 17:39:27 +0100

    Add crawler module (#12)
    * Add crawler module
    
    * Update package.json
    
    * Update packages/crawler/src/utils.ts
    
    Co-authored-by: Dominik Korsa <dominik.korsa@gmail.com>
    
    * Update client.ts and utils.ts
    
    * Update client.ts
    
    * Update client.ts
    
    * Update client.ts and utils.ts
    
    * Update packages/crawler/src/utils.ts
    
    Co-authored-by: Dominik Korsa <dominik.korsa@gmail.com>
    
    * Update packages/crawler/src/utils.ts
    
    Co-authored-by: Dominik Korsa <dominik.korsa@gmail.com>
    
    * coś tam coś tam
    
    * już nie chce mi się nazywać tych commitów
    
    ---------
    
    Co-authored-by: Dominik Korsa <dominik.korsa@gmail.com>

---
+++ packages/crawler/src/client.ts
@@ -0,0 +1,115 @@
+import axios, { Axios } from 'axios';
+import knex from 'knex';
+import { JSDOM } from 'jsdom';
+import { getPageType, findLinksByKeywords, fixUrl, uniqueBy, PageType } from './utils.js';
+import { parse } from '@timetable-api/optivum-scrapper';
+import { createHash } from 'crypto';
+import { SchoolsTable, OptivumTimetableVersionsTable, TimetableUrlsTable } from '@timetable-api/common';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const dbClient = knex({
+    client: 'pg',
+    version: '7.2',
+    connection: {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: Number(process.env.DB_PORT),
+    },
+    useNullAsDefault: true,
+});
+
+export async function run() {
+    const axiosInstance = axios.create();
+    const schools = await dbClient<SchoolsTable & { website_url: string }>('schools').whereNotNull('website_url');
+    schools.map(async (school) => {
+        console.log(`[RSPO ID: ${school.rspo_id}] Getting timetables...`);
+        const timetables = await getTimetables(axiosInstance, school.website_url);
+        if (timetables.length !== 0) {
+            console.log(`[RSPO ID: ${school.rspo_id}] Pushing data to database...`);
+        }
+        timetables.map(async (timetable) => {
+            const hash = createHash('sha512').update(JSON.stringify(timetable.data.htmls)).digest('hex');
+            const timetableVersion = await dbClient<OptivumTimetableVersionsTable, { unqiue_id: number }>(
+                'optivum_timetable_versions',
+            )
+                .returning('unique_id')
+                .insert({
+                    school_rspo_id: school.rspo_id,
+                    generated_on: timetable.data.data.generationDate,
+                    timetable_data: JSON.stringify(timetable.data.data),
+                    discriminant: dbClient.raw(
+                        `(SELECT coalesce(max(discriminant), -1) + 1 FROM optivum_timetable_versions WHERE school_rspo_id = ${school.rspo_id} AND generated_on = '${timetable.data.data.generationDate}')`,
+                    ),
+                    hash,
+                })
+                .onConflict(['school_rspo_id', 'hash'])
+                .merge(['school_rspo_id'])
+                .returning('*')
+                .first();
+            await dbClient<TimetableUrlsTable, { id: number }[]>('timetable_urls')
+                .returning('id')
+                .insert({
+                    timetable_version_id: timetableVersion.unique_id,
+                    school_rspo_id: school.rspo_id,
+                    url: timetable.url,
+                })
+                .onConflict(['school_rspo_id', 'url', 'timetable_id'])
+                .merge(['last_check_at']);
+        });
+        console.log(`[RSPO ID: ${school.rspo_id}] Done!`);
+    });
+}
+
+async function findTimetables(
+    depthLimit: number,
+    url: string,
+    axiosInstance: Axios,
+    checkedLinks: Set<string> = new Set<string>(),
+) {
+    const timetables: { url: string; type: PageType }[] = [];
+    let response;
+    try {
+        response = await axiosInstance.get<string>(url.replace('://www.', '://'));
+    } catch {
+        return;
+    }
+    checkedLinks.add(url);
+    const document = new JSDOM(response.data).window.document;
+    const links = [
+        ...new Set(findLinksByKeywords(document).map((link) => new URL(link, url.replace('://www.', '://')).toString())),
+    ];
+    if (depthLimit > 0) {
+        await Promise.all(
+            links.map(async (link) => {
+                if (checkedLinks.has(link)) return;
+                checkedLinks.add(link);
+                const result = await findTimetables(depthLimit - 1, link, axiosInstance, checkedLinks);
+                if (result !== undefined) {
+                    timetables.push(...result.timetables);
+                }
+            }),
+        );
+    }
+    const timetableType = getPageType(document);
+    if (timetableType !== null) timetables.push({ url, type: timetableType });
+    return { url: url.replace('://www.', '://'), timetables: uniqueBy(timetables, (el) => el.url) };
+}
+
+async function getTimetables(axiosInstance: Axios, websiteUrl: string) {
+    const timetablesInfo = await findTimetables(3, fixUrl(websiteUrl), axiosInstance);
+    if (timetablesInfo === undefined) {
+        return [];
+    }
+    try {
+        const promises = timetablesInfo.timetables
+            .filter((timetableInfo) => timetableInfo.type === 'optivum')
+            .map(async (timetableInfo) => ({ data: await parse(timetableInfo.url), url: timetableInfo.url }));
+        return await Promise.all(promises);
+    } catch {
+        return [];
+    }
+}
---
+++ packages/crawler/src/utils.ts
@@ -0,0 +1,80 @@
+const keywords = [
+    'plan',
+    'harmonogram',
+    'tutaj',
+    'kliknij',
+    'naciśnij',
+    'nacisnij',
+    'podzial',
+    'podział',
+    'rozkład',
+    'rozklad',
+    'timetable',
+    'lekcj',
+    'schedule',
+];
+
+export function findLinksByKeywords(document: Document): string[] {
+    const links = new Set<string>();
+    Array.from(document.querySelectorAll('a[href]')).forEach((link) => {
+        if (
+            keywords.some(
+                (keyword) =>
+                    link.textContent?.toLowerCase()?.includes(keyword) === true ||
+                    link.querySelector('img')?.getAttribute('src')?.toLowerCase()?.includes(keyword) === true ||
+                    link.querySelector('img')?.getAttribute('alt')?.toLowerCase()?.includes(keyword) === true ||
+                    link.getAttribute('href')!.toLowerCase().includes(keyword),
+            )
+        )
+            links.add(link.getAttribute('href')!);
+    });
+    Array.from(document.querySelectorAll('iframe[src]')).forEach((link) => {
+        links.add(link.getAttribute('src')!);
+    });
+    Array.from(document.querySelectorAll('script')).forEach((element) => {
+        element.innerHTML
+            .replace(/\\(.)/gm, '$1')
+            .match(/(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-&?=%.]+/gm)
+            ?.forEach((link) => {
+                if (keywords.find((keyword) => link.toLowerCase().includes(keyword)) !== undefined) {
+                    links.add(link);
+                }
+            });
+    });
+    return [...links].map(link => link.replace('://www.', '://'));
+}
+
+export type PageType = 'optivum' | 'asctimetables';
+
+function pageIsAsc(page: Document) {
+    return (
+        page.querySelector(
+            'a[style="color:inherit"][target="_blank"][href="http://www.asctimetables.com/timetables_pl.html"]',
+        )?.textContent === 'aSc Plan Lekcji - program do tworzenia planu lekcji'
+    );
+}
+function pageIsOptivum(page: Document) {
+    const content = page.querySelector('meta[name="description"]')?.getAttribute('content') ?? null;
+    if (content === null) return false;
+    return content.includes('programu Plan lekcji Optivum firmy VULCAN') || content.includes('Plan lekcji w szkole');
+}
+export function getPageType(page: Document): PageType | null {
+    if (pageIsAsc(page)) return 'asctimetables';
+    if (pageIsOptivum(page)) return 'optivum';
+    return null;
+}
+
+export const fixUrl = (url: string) =>
+    !url.includes('://') ? 'http://' + url.replace('://www.', '://') : url.replace('://www.', '://');
+
+export function uniqueBy<T, H extends string | number>(array: T[], getHash: (el: T) => H) {
+  const existingHashes = new Set<H>();
+  const result: T[] = [];
+  array.forEach((el) => {
+    const hash = getHash(el);
+    if (existingHashes.has(hash)) return;
+    existingHashes.add(hash);
+    result.push(el);
+  });
+  return result;
+};
