e629e971a4e2a7d48f5edd847c4a83a3c298d36a
Marioneq 4958 <mq4958@gmail.com>
2024-01-26 09:31:41 +0100

    Update client.ts

--- packages/schools/src/client.ts
+++ packages/schools/src/client.ts
@@ -77,5 +77,5 @@ function checkUrl(url: string) {
     if (url === '' || !/(^https?:\/\/)?((?:[a-z0-9-]+\.)+[a-z][a-z0-9-]*)(:\d{1,5})?(\/.*)?$/m.test(url)) {
         return null;
     }
-    return url.includes('://') ? 'http://' + url.replace('www.', '') : url.replace('www.', '');
+    return url.includes('://') ? url.replace('://www.', '://') : 'http://' + url.replace('://www.', '://');
 }
