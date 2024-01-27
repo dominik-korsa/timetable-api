b7d86e93a8e01858a4d7a7432fee96f11219a7b9
Marioneq 4958 <mq4958@gmail.com>
2024-01-27 15:43:25 +0100

    Update crawler client

--- packages/crawler/src/client.ts
+++ packages/crawler/src/client.ts
@@ -5,27 +5,36 @@ import { parse } from '@timetable-api/optivum-scrapper';
 import { createHash } from 'crypto';
 import { SchoolsTable } from '@timetable-api/common';
 import { getSchoolsWithWebiste, pushOptivumTimetableVersion, pushOptivumTimetableVersionUrl } from './db.js';
+import { SingleBar } from 'cli-progress';
 
 export async function run() {
     const axiosInstance = axios.create();
     const schools = await getSchoolsWithWebiste();
-    schools.map(async (school) => checkSchool(school, axiosInstance));
+    schools.map((school) => checkSchool(school, axiosInstance));
 }
 
-async function checkSchool(school: SchoolsTable & {website_url: string}, axiosInstance: Axios) {
-    console.log(`[RSPO ID: ${school.rspo_id}] Getting timetables...`);
-    const { optivumTimetables } = await findTimetables(school.website_url, axiosInstance);
-    if (optivumTimetables.length !== 0) {
-        console.log(`[RSPO ID: ${school.rspo_id}] Pushing data to database...`);
+async function checkSchool(school: SchoolsTable & { website_url: string }, axiosInstance: Axios) {
+    const line = new SingleBar({ format: '[RSPO_ID: {rspo_id}] {status}', noTTYOutput: true });
+    line.start(1, 0, { status: 'Getting timetables...', rspo_id: school.rspo_id });
+    let optivumTimetables: string[];
+    try {
+        optivumTimetables = (await findTimetables(school.website_url, axiosInstance)).optivumTimetables;
+    } catch {
+        return;
     }
-    optivumTimetables.map(async (timetableUrl) => {
-        const parsedTimetable = await parse(timetableUrl, axiosInstance)
-        const hash = createHash('sha512').update(JSON.stringify(parsedTimetable.htmls)).digest('hex');
-        const timetableVersion = await pushOptivumTimetableVersion(parsedTimetable, school.rspo_id, hash);
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        await pushOptivumTimetableVersionUrl(timetableUrl, timetableVersion!.unique_id, school.rspo_id)
-    });
-    console.log(`[RSPO ID: ${school.rspo_id}] Done!`);
+    if (optivumTimetables.length !== 0) {
+        line.update(0, { status: 'Parsing timetables and pushing data to database...' });
+    }
+    await Promise.allSettled(
+        optivumTimetables.map(async (timetableUrl) => {
+            const parsedTimetable = await parse(timetableUrl, axiosInstance);
+            const hash = createHash('sha512').update(JSON.stringify(parsedTimetable.htmls)).digest('hex');
+            const timetableVersion = await pushOptivumTimetableVersion(parsedTimetable, school.rspo_id, hash);
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            await pushOptivumTimetableVersionUrl(timetableUrl, timetableVersion!.unique_id, school.rspo_id);
+        }),
+    );
+    line.update(0, { status: 'Done!' });
 }
 
 async function findTimetables(
@@ -43,7 +52,7 @@ async function findTimetables(
         return {url, optivumTimetables: []};
     }
     checkedLinks.add(url);
-    const document = new JSDOM(response.data).window.document;
+    const document = new JSDOM(response.data.replace(/<style(\s|>).*?<\/style>/gi, '')).window.document;
     const links = [
         ...new Set(findLinksByKeywords(document).map((link) => new URL(link, url).toString())),
     ];
