6eb970355929f57dc54d93d58718c0fccc14877c
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2024-02-17 10:56:53 +0100

    Add x-content-type-options header

--- packages/api/src/main.rs
+++ packages/api/src/main.rs
@@ -12,17 +12,32 @@ use crate::routes::docs::{create_docs_router, redirect_to_docs, transform_api_do
 use crate::routes::schools::create_schools_router;
 use aide::axum::ApiRouter;
 use aide::openapi::OpenApi;
-use axum::response::IntoResponse;
+use axum::response::{IntoResponse, Response};
 use axum::routing::get;
 use axum::Extension;
 use dotenvy::dotenv;
 use std::env;
 use std::net::SocketAddr;
 use std::sync::Arc;
+use axum::extract::Request;
+use axum::http::header::X_CONTENT_TYPE_OPTIONS;
+use axum::http::HeaderValue;
+use axum::middleware::Next;
+use tower::ServiceBuilder;
 use tower_http::cors::{Any, CorsLayer};
 
 async fn handle_fallback() -> impl IntoResponse {
     ApiError::RouteNotFound
+}
+
+async fn default_headers_middleware(
+    request: Request,
+    next: Next,
+) -> Response {
+    let mut response = next.run(request).await;
+    let headers = response.headers_mut();
+    headers.insert(X_CONTENT_TYPE_OPTIONS, HeaderValue::from_static("nosniff"));
+    response
 }
 
 #[tokio::main]
@@ -42,6 +57,9 @@ async fn main() {
 
     let cors = CorsLayer::new()
         .allow_origin(Any);
+    let headers_layer = ServiceBuilder::new()
+        .layer(cors)
+        .layer(axum::middleware::from_fn(default_headers_middleware));
 
     let app_router = ApiRouter::new()
         .merge(create_schools_router())
@@ -51,7 +69,7 @@ async fn main() {
         .finish_api_with(&mut api, transform_api_docs)
         .with_state(db)
         .layer(Extension(Arc::new(api)))
-        .layer(cors)
+        .layer(headers_layer)
         .into_make_service_with_connect_info::<SocketAddr>();
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
