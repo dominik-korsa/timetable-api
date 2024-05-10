b1ecac21fdd5d409ab4d00d56bdad622b49be8f5
dominik-korsa <29484605+dominik-korsa@users.noreply.github.com>
2024-05-10 22:07:38 +0200

    Change institution type ids in schools package

--- packages/schools/src/client.ts
+++ packages/schools/src/client.ts
@@ -87,3 +87,9 @@ function checkUrl(url: string) {
     }
     return url.includes('://') ? url.replace('://www.', '://') : 'http://' + url.replace('://www.', '://');
 }
+
+run([
+    93, 94, 97, 45, 96, 4, 89, 34, 65, 14, 17, 15, 27, 54, 75, 83, 64, 29, 21, 24, 26, 47, 74, 46, 91, 92, 51, 50, 85, 86, 3, 19, 20, 16, 100
+])
+    .then(() => { process.exit(); })
+    .catch(console.error);
