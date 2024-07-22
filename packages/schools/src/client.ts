a93419338bc02b7d0a6218afefec35e21e72577b
Marioneq 4958 <mq4958@gmail.com>
2024-07-22 13:49:21 +0200

    Improve extractInstitutionId and checkUrl

--- packages/schools/src/client.ts
+++ packages/schools/src/client.ts
@@ -25,7 +25,7 @@ export async function run(institutionTypeIds: number[]): Promise<void> {
             let page = 1;
             // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, no-constant-condition
             while (true) {
-                console.log(`[Institution type: ${institutionTypeId}, page: ${page}] Fetching data from Rspo API...`);
+                console.log(`[Institution type: ${institutionTypeId}, page: ${page}] Fetching data from RSPO API...`);
                 let data: Institution[] | undefined;
                 let nextPageAvalible: boolean | undefined;
                 try {
@@ -60,7 +60,7 @@ export async function run(institutionTypeIds: number[]): Promise<void> {
                             corresp_addr_zip_code: school.adresDoKorespondecjiKodPocztowy,
                             website_url: checkUrl(school.stronaInternetowa),
                             institution_type: school.typ.id,
-                            parent_rspo_id: extractInstitutionId(school.podmiotNadrzedny),
+                            parent_rspo_id: school.podmiotNadrzedny !== null ? extractInstitutionId(school.podmiotNadrzedny) : null,
                         })),
                     )
                     .onConflict('rspo_id')
@@ -75,22 +75,18 @@ export async function run(institutionTypeIds: number[]): Promise<void> {
     console.log('Done!');
 }
 
-function extractInstitutionId(url: string | null): string | null {
-    if (url === null) return null;
+function extractInstitutionId(url: string): string {
     const prefix = '/api/placowki/';
     if (!url.startsWith(prefix)) throw new Error(`Institution URL does not start with '${prefix}'`);
     return url.substring(prefix.length);
 }
 
-function checkUrl(url: string) {
-    if (url === '' || !/(^https?:\/\/)?((?:[a-z0-9-]+\.)+[a-z][a-z0-9-]*)(:\d{1,5})?(\/.*)?$/m.test(url)) {
-        return null;
-    }
-    return url.includes('://') ? url.replace('://www.', '://') : 'http://' + url.replace('://www.', '://');
-}
+const urlRegex = /(^https?:\/\/)?((?:[a-z0-9-]+\.)+[a-z][a-z0-9-]*)(:\d{1,5})?(\/.*)?$/m;
 
-run([
-    93, 94, 97, 45, 96, 4, 89, 34, 65, 14, 17, 15, 27, 54, 75, 83, 64, 29, 21, 24, 26, 47, 74, 46, 91, 92, 51, 50, 85, 86, 3, 19, 20, 16, 100
-])
-    .then(() => { process.exit(); })
-    .catch(console.error);
+function checkUrl(url: string): string | null {
+    if (url === '' || !urlRegex.test(url))
+        return null;
+    if (!url.includes('://')) url = 'http://' + url;
+    url.replace('://www.', '://');
+    return url;
+}
--- packages/schools/src/index.ts
+++ packages/schools/src/index.ts
@@ -1,3 +1,7 @@
-export * from './client.js';
-export * from './rspo/client.js';
-export * from './rspo/types.js';
+import { run } from "./client.js";
+
+run([
+    93, 94, 97, 45, 96, 4, 89, 34, 65, 14, 17, 15, 27, 54, 75, 83, 64, 29, 21, 24, 26, 47, 74, 46, 91, 92, 51, 50, 85, 86, 3, 19, 20, 16, 100
+])
+    .then(() => { process.exit(); })
+    .catch(console.error);
