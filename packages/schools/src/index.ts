06c68aef4b3b8dc1f22ee43e5d0ca0378abbbcdc
Marioneq 4958 <mq4958@gmail.com>
2024-07-22 14:00:26 +0200

    Add descriptions for institution types in index.ts

--- packages/schools/src/index.ts
+++ packages/schools/src/index.ts
@@ -1,7 +1,42 @@
-import { run } from "./client.js";
+import { run } from './client.js';
 
 run([
-    93, 94, 97, 45, 96, 4, 89, 34, 65, 14, 17, 15, 27, 54, 75, 83, 64, 29, 21, 24, 26, 47, 74, 46, 91, 92, 51, 50, 85, 86, 3, 19, 20, 16, 100
+    93, // Branżowa szkoła I stopnia
+    94, // Branżowa szkoła II stopnia
+    97, // Branżowe Centrum Umiejętności
+    45, // Centrum Kształcenia Praktycznego
+    96, // Centrum Kształcenia Zawodowego
+    89, // Inna szkoła artystyczna
+    34, // Kolegium nauczycielskie
+    65, // Kolegium Pracowników Służb Społecznych
+    14, // Liceum ogólnokształcące
+    17, // Liceum ogólnokształcące uzupełniające dla absolwentów zasadniczych szkół zawodowych
+    15, // Liceum profilowane
+    27, // Liceum sztuk plastycznych
+    54, // Młodzieżowy Ośrodek Socjoterapii ze szkołami
+    75, //Niepubliczna placówka kształcenia ustawicznego i praktycznego
+    83, // Niepubliczna placówka kształcenia ustawicznego i praktycznego ze szkołami
+    64, // Niepubliczna placówka oświatowo-wychowawcza w systemie oświaty
+    29, // Ogólnokształcąca szkoła baletowa
+    21, // Ogólnokształcąca szkoła muzyczna I stopnia
+    24, // Ogólnokształcąca szkoła muzyczna II stopnia
+    26, // Ogólnokształcąca szkoła sztuk pięknych
+    47, // Ośrodek dokształcania i doskonalenia zawodowego
+    74, // Placówka Kształcenia Ustawicznego - bez szkół
+    46, // Placówka Kształcenia Ustawicznego ze szkołami
+    91, // Policealna szkoła muzyczna
+    92, // Policealna szkoła plastyczna
+    51, // Specjalny Ośrodek Szkolno-Wychowawczy
+    50, // Specjalny Ośrodek Wychowawczy
+    85, // Szkoła muzyczna I stopnia
+    86, // Szkoła muzyczna II stopnia
+    3, // Szkoła podstawowa
+    19, // Szkoła policealna
+    20, // Szkoła specjalna przysposabiająca do pracy
+    16, // Technikum
+    100, // Zespół szkół i placówek oświatowych
 ])
-    .then(() => { process.exit(); })
+    .then(() => {
+        process.exit();
+    })
     .catch(console.error);
