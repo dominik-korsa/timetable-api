3ec124741f953e2c196c7fcab155a22b0602b4ff
Marioneq 4958 <mq4958@gmail.com>
2024-07-22 14:04:24 +0200

    Use connection string in connecting to database

--- packages/schools/src/client.ts
+++ packages/schools/src/client.ts
@@ -9,11 +9,7 @@ const dbClient = knex({
     client: 'pg',
     version: '7.2',
     connection: {
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: Number(process.env.DB_PORT),
+        connectionString: process.env.DATABASE_URL
     },
     useNullAsDefault: true,
 });
