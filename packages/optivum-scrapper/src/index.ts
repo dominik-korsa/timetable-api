6a2dd83d5a95bc7075af4a75d07ed1801e82c7a3
Marioneq 4958 <mq4958@gmail.com>
2024-11-23 15:40:35 +0100

    Create iterator into candidates

--- packages/optivum-scrapper/src/index.ts
+++ packages/optivum-scrapper/src/index.ts
@@ -1,10 +1,23 @@
+import { asyncForEachWithLimit, ParalelLimit } from '@timetable-api/common';
 import { getOptivumCandidates } from './db.js';
 
 export * from './client.js';
 
+const PARALEL_CANDIDATE_LIMIT = 30; // TODO: Set optimal value
+
 async function main() {
-    console.log('Downloading optivum candidates list...')
-    await getOptivumCandidates();
+    console.log('Downloading optivum candidates list...');
+    const candidates = await getOptivumCandidates();
+
+    console.log('Handling candidates...');
+    await asyncForEachWithLimit(
+        candidates,
+        // eslint-disable-next-line @typescript-eslint/require-await
+        async (candidate) => {
+            console.log(candidate.id);
+        },
+        new ParalelLimit(PARALEL_CANDIDATE_LIMIT),
+    );
 }
 
 main()
