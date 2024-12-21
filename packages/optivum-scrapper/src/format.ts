2b593bd45d0b7d6df4e1a7739fa0c12be158f2c5
Marioneq 4958 <mq4958@gmail.com>
2024-12-21 12:51:19 +0100

    Improvements for optivum-scrapper (#52)
    * .
    
    * Use index for timeSlots, days, weeks and periods. Add beginMinute and endMinute in lesson.
    
    * Next big commit
    
    * Update optivum scrapper and crawler run script
    
    * Update packages/optivum-scrapper/src/timetable.ts
    
    Co-authored-by: Dominik <29484605+dominik-korsa@users.noreply.github.com>
    
    * Improve
    
    ---------
    
    Co-authored-by: Dominik <29484605+dominik-korsa@users.noreply.github.com>

---
+++ packages/optivum-scrapper/src/format.ts
@@ -0,0 +1,118 @@
+import { TimetableClass, TimetableTeacher, TimetableRoom } from '@timetable-api/common';
+import { ClientLesson, CommonGroup, Day, ParseResult, TimeSlot, UnitType } from './types.js';
+import { DefaultsMap, getUnitName } from './utils.js';
+
+const mapDay = ({ name, isoNumber }: Day) => ({ short: null, name, isoNumber });
+
+const mapTimeSlot = ({ name, beginMinute, endMinute }: TimeSlot) => ({ name, beginMinute, endMinute });
+
+const mapInterclassGroup = ([id, classIds]: [string, string[]]) => ({ id, classIds });
+
+const mapSubject = (id: string) => ({ id, short: id, name: null, color: null });
+
+const mapCommonGroup = ([id, { groupShort, subjectId, classId }]: [
+    string,
+    { groupShort: string; subjectId: string; classId: string },
+]) => ({ id, short: groupShort, subjectId, classId, color: null });
+
+const mapClass = ({ id, short, fullName }: { id: string; short: string; fullName: string | null }) => ({
+    id,
+    short,
+    fullName,
+    name: fullName !== null ? getUnitName(fullName, short, UnitType.CLASS) : null,
+    color: null,
+    teacherId: null,
+});
+
+const mapTeacher = ({ id, short, fullName }: { id: string; short: string; fullName: string | null }) => ({
+    id,
+    short,
+    fullName,
+    name: fullName !== null ? getUnitName(fullName, short, UnitType.TEACHER) : null,
+    color: null,
+});
+
+const mapRoom = ({ id, short, fullName }: { id: string; short: string; fullName: string | null }) => ({
+    id,
+    short,
+    fullName,
+    name: fullName !== null ? getUnitName(fullName, short, UnitType.ROOM) : null,
+    color: null,
+    buildingId: null,
+});
+
+const formatLessons = (lessons: DefaultsMap<string, ClientLesson[]>, timeSlots: TimeSlot[]) =>
+    [...lessons.entries()].flatMap(([index, cell]) => {
+        const [dayIndex, timeSlotIndex] = index.split('|').map(Number);
+        const timeSlot = timeSlots[timeSlotIndex];
+        return cell.map((lesson) => {
+            const isDefaultLesson = lesson.type === 'default';
+            return {
+                timeSlotIndex,
+                dayIndex,
+                beginMinute: timeSlot.beginMinute,
+                endMinute: timeSlot.endMinute,
+                comment: !isDefaultLesson ? lesson.comment : null,
+                subjectId: isDefaultLesson ? lesson.subjectId : null,
+                teacherIds: isDefaultLesson && lesson.teacherId !== null ? [lesson.teacherId] : [],
+                roomIds: isDefaultLesson && lesson.roomId !== null ? [lesson.roomId] : [],
+                groupIds: isDefaultLesson ? [...lesson.groupIds] : [],
+                classIds: isDefaultLesson ? [...lesson.classIds] : [],
+                interclassGroupId: isDefaultLesson ? lesson.interclassGroupId : null,
+                weekIndex: null,
+                periodIndex: null,
+                seminarGroup: null,
+                studentIds: [],
+            };
+        });
+    });
+
+const formatUnits = (shorts: Map<string, string>, fullNames: Map<string, string>, type: UnitType) =>
+    [...shorts]
+        .sort((a, b) => Number(a[0]) - Number(b[0]))
+        .map(([id, short]) => {
+            const fullName = fullNames.get(type + id) ?? null;
+            switch (type) {
+                case UnitType.CLASS:
+                    return mapClass({ id, short, fullName });
+                case UnitType.TEACHER:
+                    return mapTeacher({ id, short, fullName });
+                case UnitType.ROOM:
+                    return mapRoom({ id, short, fullName });
+            }
+        });
+
+export const formatResult = (
+    days: Day[],
+    timeSlots: TimeSlot[],
+    classShorts: Map<string, string>,
+    teacherShorts: Map<string, string>,
+    roomShorts: Map<string, string>,
+    unitFullNames: Map<string, string>,
+    interclassGroups: Map<string, string[]>,
+    subjects: Set<string>,
+    commonGroups: Map<string, CommonGroup>,
+    lessons: DefaultsMap<string, ClientLesson[]>,
+    generatedDate: string,
+    validFrom: string | null,
+): ParseResult => ({
+    data: {
+        common: {
+            days: days.map(mapDay),
+            timeSlots: timeSlots.map(mapTimeSlot),
+            classes: formatUnits(classShorts, unitFullNames, UnitType.CLASS) as TimetableClass[],
+            teachers: formatUnits(teacherShorts, unitFullNames, UnitType.TEACHER) as TimetableTeacher[],
+            rooms: formatUnits(roomShorts, unitFullNames, UnitType.ROOM) as TimetableRoom[],
+            interclassGroups: [...interclassGroups].map(mapInterclassGroup),
+            subjects: [...subjects].map(mapSubject),
+            commonGroups: [...commonGroups].map(mapCommonGroup),
+            buildings: [],
+            periods: [],
+            weeks: [],
+            students: [],
+        },
+        lessons: formatLessons(lessons, timeSlots),
+    },
+    generatedDate,
+    validFrom,
+});
--- packages/optivum-scrapper/src/timetable.ts
+++ packages/optivum-scrapper/src/timetable.ts
@@ -1,118 +1,240 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { Axios, AxiosResponse } from 'axios';
-import { AxiosCacheInstance } from 'axios-cache-interceptor';
-import { JSDOM } from 'jsdom';
-import { Table } from './table.js';
-import { Unit, UnitType } from './types.js';
-import { parseUnitLink } from './utils.js';
+import { Axios } from 'axios';
+import { ClientLesson, CommonGroup, Day, LessonTimeSlot, TimeSlot, Unit, UnitList, UnitType } from './types.js';
+import { DefaultsMap, getDocument, parseUnitLink } from './utils.js';
 import { isDefined } from '@timetable-api/common';
+import { Table } from './table.js';
+import { createHash } from 'crypto';
+import { formatResult } from './format.js';
 
 export class Timetable {
     private baseUrl: string;
-    private readonly axios: Axios | AxiosCacheInstance;
+    private readonly axios: Axios;
+    private unitListPromise: Promise<UnitList> | null;
+    private unitHTMLsPromise: Promise<(Unit & { html: string })[]> | null;
 
-    constructor(baseUrl: string, axios: Axios | AxiosCacheInstance) {
+    constructor(baseUrl: string, axios: Axios, unitList?: UnitList, unitHTMLs?: (Unit & { html: string })[]) {
         this.baseUrl = baseUrl;
         this.axios = axios;
+        this.unitListPromise = unitList ? Promise.resolve(unitList) : null;
+        this.unitHTMLsPromise = unitHTMLs ? Promise.resolve(unitHTMLs) : null;
     }
 
-    public async getDocument(path: string): Promise<{
-        response: string;
-        responseUrl: string;
-    }> {
+    private async fetchDocument(path: string) {
         const url = new URL(path, this.baseUrl).toString();
-        const response: AxiosResponse = await this.axios.get(url, {
-            headers: {
-                'User-Agent': 'Timetable-Api',
-            },
-            cache: false,
-        });
+        const response = await this.axios.get<string>(url);
         return {
-            response: response.data as string,
+            response: response.data,
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-            responseUrl: response.request.res.responseUrl as string | null ?? url,
+            responseUrl: (response.request.res.responseUrl as string | null) ?? url,
         };
     }
 
-    public async getTable(type: UnitType, id: string): Promise<Table> {
-        const { response } = await this.getDocument(`plany/${type}${id}.html`);
-        return new Table(response);
+    getUnitList() {
+        if (!this.unitListPromise) this.unitListPromise = this.fetchAndParseUnitList();
+        return this.unitListPromise;
     }
 
-    private async getIndexPageFromScript() {
-        const scriptResponse = (await this.getDocument('../scripts/powrot.js')).response;
-        const { response, responseUrl } = await this.getDocument(
-            /<a href="(.*?)">Plan lekcji<\/a>/.exec(scriptResponse)?.[1] ?? '../index.html',
-        );
-        return [response, responseUrl];
-    }
-
-    private async getListPageFromFrame(frame: Element) {
-        const listPath = frame.getAttribute('src')!;
-        const { response, responseUrl } = await this.getDocument(listPath);
-        return [response, responseUrl];
-    }
-
-    public async getUnitList(): Promise<{ units: Unit[]; sources: string[] }> {
-        const { response, responseUrl } = await this.getDocument(this.baseUrl);
+    private async fetchAndParseUnitList() {
+        const { response, responseUrl } = await this.fetchDocument(this.baseUrl);
         this.baseUrl = responseUrl;
-        let document = new JSDOM(response).window.document;
-        if (document.querySelector('script[src="../scripts/powrot.js"]') !== null) {
-            const [response, responseUrl] = await this.getIndexPageFromScript();
+        let document = getDocument(response);
+
+        if (document.querySelector('script[src="../scripts/powrot.js"]')) {
+            const { response, responseUrl } = await this.getIndexPageFromScript();
             this.baseUrl = responseUrl;
-            document = new JSDOM(response).window.document;
+            document = getDocument(response);
         }
-        if (document.querySelector('.menu') !== null) {
-            const list: { units: Unit[]; sources: string[] } = { units: [], sources: [] };
-            await Promise.all(
-                [...document.querySelectorAll('a[hidefocus="true"][href]')].map(async (listLink) => {
-                    const href = listLink.getAttribute('href')!;
-                    const { response, responseUrl } = await this.getDocument(href);
-                    const unitList = this.parseUnitList(response);
-                    list.units.push(...unitList);
-                    list.sources.push(responseUrl);
-                }),
-            );
-            list.sources.sort();
-            return list;
-        }
-        const frame = document.querySelector('frame[name="list"][src]');
+
+        if (document.querySelector('.menu')) return await this.handleMulitpageUnitList(document);
+
+        const frame = document.querySelector<HTMLFrameElement>('frame[name="list"][src]');
         if (frame) {
-            const [response, responseUrl] = await this.getListPageFromFrame(frame);
+            const { response, responseUrl } = await this.getListPageFromFrame(frame);
             this.baseUrl = responseUrl;
-            document = new JSDOM(response).window.document;
+            document = getDocument(response);
         }
-        return { units: this.parseUnitList(document.documentElement.innerHTML), sources: [this.baseUrl] };
+
+        return { units: Timetable.parseUnitList(document), sources: [this.baseUrl] };
     }
 
-    public parseUnitList(html: string): Unit[] {
-        const document: Document = new JSDOM(html).window.document;
-        let units: { type: UnitType; id: string; fullName: string }[];
-        if (document.querySelector('select')) {
-            units = [...document.querySelectorAll('select')].flatMap((select) => {
-                const type = select.querySelector('option:first-child')?.textContent?.[0];
-                if (type !== 'o' && type !== 'n' && type !== 's') throw new Error('Unknown type');
-                return [...select.querySelectorAll('option:not(:first-child)')].map((option) => {
-                    const id = option.getAttribute('value');
-                    if (id === null) throw new Error('Missing option value');
-                    return {
-                        id,
-                        type,
-                        fullName: option.textContent ?? '',
-                    };
-                });
+    static readonly indexPageAnchorRegex = /<a href="(.*?)">Plan lekcji<\/a>/;
+    private async getIndexPageFromScript() {
+        const { response: scriptResponse } = await this.fetchDocument('../scripts/powrot.js');
+        return await this.fetchDocument(Timetable.indexPageAnchorRegex.exec(scriptResponse)?.[1] ?? '../index.html');
+    }
+
+    private async getListPageFromFrame(frame: HTMLFrameElement) {
+        return await this.fetchDocument(frame.src);
+    }
+
+    private async handleMulitpageUnitList(document: Document) {
+        const units: Unit[] = [];
+        const sources: string[] = [];
+        await Promise.all(
+            [...document.querySelectorAll<HTMLAnchorElement>('.menu a[href]')].map(async (listLink) => {
+                const { response, responseUrl } = await this.fetchDocument(listLink.href);
+                const listDocument = getDocument(response);
+                units.push(...Timetable.parseUnitList(listDocument));
+                sources.push(responseUrl);
+            }),
+        );
+        sources.sort();
+        return { units, sources };
+    }
+
+    private static readonly parseUnitList = (document: Document) =>
+        document.querySelector('select') ? Timetable.parseSelectList(document) : Timetable.parseNormalList(document);
+
+    private static readonly parseSelectList = (document: Document) =>
+        [...document.querySelectorAll<HTMLSelectElement>('select[name]')].flatMap((selectEl) => {
+            const type = selectEl.name[0];
+            if (!['o', 'n', 's'].includes(type)) throw new Error('Unknown type');
+            return [...selectEl.querySelectorAll<HTMLOptionElement>('option[value]')].map((optionEl) => ({
+                id: optionEl.value,
+                type: type as UnitType,
+            }));
+        });
+
+    private static readonly parseNormalList = (document: Document) =>
+        [...document.querySelectorAll('a[href]')].map(parseUnitLink).filter(isDefined);
+
+    private async getUnitHTMLs() {
+        if (!this.unitHTMLsPromise) this.unitHTMLsPromise = this.fetchUnitHTMLs();
+        return this.unitHTMLsPromise;
+    }
+
+    private async fetchUnitHTMLs() {
+        const unitList = await this.getUnitList();
+        return await Promise.all(
+            unitList.units.map(async ({ type, id }) =>
+                this.fetchUnitHTML(type, id).then((html) => ({ type, id, html })),
+            ),
+        );
+    }
+
+    private async fetchUnitHTML(type: UnitType, id: string) {
+        const { response } = await this.fetchDocument(`plany/${type}${id}.html`);
+        return response;
+    }
+
+    async getHash() {
+        const unitHTMLs = await this.getUnitHTMLs();
+        return createHash('sha512')
+            .update(JSON.stringify([...unitHTMLs.values()].sort()))
+            .digest('hex');
+    }
+
+    async parse() {
+        const unitHTMLs = await this.getUnitHTMLs();
+        let generatedDate: string | null = null;
+        let validFrom: string | null = null;
+        const unitFullNames = new Map<string, string>();
+        let classShorts = new Map<string, string>();
+        let teacherShorts = new Map<string, string>();
+        let roomShorts = new Map<string, string>();
+        let days: Day[] = [];
+        let timeSlots: TimeSlot[] = [];
+        let subjects = new Set<string>();
+        const interclassGroups = new Map<string, string[]>();
+        let commonGroups = new Map<string, CommonGroup>();
+        const lessons = new DefaultsMap<string, ClientLesson[]>(() => []);
+
+        unitHTMLs.forEach((unit) => {
+            const table = new Table(unit.html, unit.id, unit.type);
+
+            unitFullNames.set(unit.type + unit.id, table.getTitle());
+
+            if (generatedDate === null) {
+                generatedDate = table.getGeneratedDate();
+                validFrom = table.getValidationDate();
+                days = table.getDays();
+            }
+            if (table.getRowsLength() > timeSlots.length) timeSlots = table.getTimeSlots();
+
+            const parsedTable = table.parseMainTable();
+
+            parsedTable.subjects.forEach((subjectId) => subjects.add(subjectId));
+            parsedTable.classShorts.forEach((short, id) => classShorts.set(id, short));
+            parsedTable.teacherShorts.forEach((short, id) => teacherShorts.set(id, short));
+            parsedTable.roomShorts.forEach((short, id) => roomShorts.set(id, short));
+            parsedTable.groups.forEach((value, id) => commonGroups.set(id, value));
+            
+            parsedTable.interclassGroupIds.forEach((id) => {
+                const existingInterclassGroup = interclassGroups.get(id);
+                if (existingInterclassGroup) existingInterclassGroup.push(unit.id);
+                else interclassGroups.set(id, [unit.id]);
             });
-        } else {
-            const links = document.querySelectorAll('a');
-            units = [...links]
-                .map((link) => {
-                    const parsedLink = parseUnitLink(link);
-                    return parsedLink
-                        ? { id: parsedLink.id, type: parsedLink.type, fullName: link.textContent! }
-                        : null;
-                })
-                .filter(isDefined);
-        }
-        return units;
+
+            parsedTable.lessons.forEach((lesson) => Timetable.handleLesson(unit, lesson, lessons));
+        });
+
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+        if (generatedDate === null) throw new Error('Missing generated date');
+
+        return formatResult(
+            days,
+            timeSlots,
+            classShorts,
+            teacherShorts,
+            roomShorts,
+            unitFullNames,
+            interclassGroups,
+            subjects,
+            commonGroups,
+            lessons,
+            generatedDate,
+            validFrom,
+        );
+    }
+
+    private static handleLesson(unit: Unit, lesson: LessonTimeSlot, lessons: DefaultsMap<string, ClientLesson[]>) {
+        const timeSlotLessons = lessons.get(`${lesson.dayIndex.toString()}|${lesson.timeSlotIndex.toString()}`);
+
+        if (lesson.type === 'comment') return timeSlotLessons.push({ type: 'comment', comment: lesson.comment });
+
+        const isMatchingLesson = (existingLesson: ClientLesson): boolean => {
+            // Comment
+            if (existingLesson.type === 'comment') return false;
+
+            // Lessons without interclass groups (by teacher)
+            if (lesson.teacherId !== null && existingLesson.teacherId !== null)
+                return lesson.teacherId === existingLesson.teacherId;
+
+            // Interclass groups
+            if (lesson.subjectId !== existingLesson.subjectId) return false;
+            // Interclass groups: class tables
+            if (lesson.interclassGroupId !== null && existingLesson.interclassGroupId !== null)
+                return lesson.interclassGroupId === existingLesson.interclassGroupId;
+            // Interclass groups: teacher tables
+            if (unit.type !== UnitType.CLASS)
+                return [...existingLesson.classIds.values()].find((classId) => lesson.classIds.has(classId));
+            
+            return false;
+        };
+        const existingLesson = timeSlotLessons.find(isMatchingLesson);
+
+        if (!existingLesson)
+            return timeSlotLessons.push({
+                type: 'default',
+                classIds: lesson.classIds,
+                teacherId: lesson.teacherId,
+                roomId: lesson.roomId,
+                subjectId: lesson.subjectId,
+                interclassGroupId: lesson.interclassGroupId,
+                groupIds: lesson.groupIds,
+            });
+        if (existingLesson.type === 'comment') return;
+
+        if (
+            existingLesson.interclassGroupId !== null &&
+            unit.type === UnitType.TEACHER &&
+            existingLesson.teacherId === null
+        )
+            existingLesson.teacherId = unit.id;
+        else if (lesson.interclassGroupId !== null && existingLesson.interclassGroupId === null)
+            existingLesson.interclassGroupId = lesson.interclassGroupId;
+        lesson.classIds.forEach((classId) => existingLesson.classIds.add(classId));
+        lesson.groupIds.forEach((groupId) => existingLesson.groupIds.add(groupId));
     }
 }
